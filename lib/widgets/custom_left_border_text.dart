7cfa2235e4686aefea1a6c9ab9ecc94fe4cf2c8e
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-03-20 23:56:57 -0300

    create custom_left_border_text widget

---
+++ lib/widgets/custom_left_border_text.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CustomLeftBorderText extends StatelessWidget {
+  final String text;
+
+  const CustomLeftBorderText({
+    Key? key, required this.text
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border(
+              left: BorderSide(
+        color: Colors.deepPurple[200]!,
+        width: 4,
+      ))),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 16),
+        child: Text(
+          text,
+          textAlign: TextAlign.justify,
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.grey[800],
+          ),
+        ),
+      ),
+    );
+  }
+}
